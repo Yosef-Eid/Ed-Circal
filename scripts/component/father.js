08b447c75e4d226b264c167d0c37eb5771280358
Yosef-Eid <yosefeid058@gmail.com>
2023-01-08 21:10:44 +0200

    sction 3

--- scripts/component/father.js
+++ scripts/component/father.js
@@ -1,8 +1,9 @@
 import comket from "../../comket/html.js";
 import OurServices from "../component/OurServices.js";
+import ExplorePrograms from "../component/ExplorePrograms.js";
 
 export default function father(){
     let father = comket.div({class:"father"})
-    father.append(OurServices(),)
+    father.append(OurServices(),ExplorePrograms())
     return father
 }
