009ea42337db8a776d01bc2483f143e47d5c5352
Yosef-Eid <yosefeid058@gmail.com>
2023-01-10 21:00:43 +0200

    Section 5

--- scripts/component/father.js
+++ scripts/component/father.js
@@ -3,9 +3,10 @@ import Section1 from "../component/Section1.js";
 import Section2 from "./Section2.js";
 import Section3 from "./Section3.js"; 
 import Section4 from "./Section4.js";
+import Section5 from "./Section5.js";
 
 export default function father(){
     let father = comket.div({class:"father"})
-    father.append(Section1(),Section2(),Section3(),Section4())
+    father.append(Section1(),Section2(),Section3(),Section4(),Section5())
     return father
 }
